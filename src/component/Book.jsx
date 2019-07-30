afeead63fa04ba0079213cae78c1d69e13ef05e1
deguchi <deguchik@gmail.com>
2019-07-30 11:50:29 +0900

    update

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -11,23 +11,30 @@ export default class item extends Component {
         const item = this.props.item;
         const fontSize = item.height / 14;
         return (
-            <React.Fragment>
-                <div className={'item'} ref="item" key={item.isbn} id={item.id} onClick={this.onClick.bind(this)}>
-                    {this.props.item.cover ? (
-                        <img src={item.cover} alt={item.title} ref="cover" />
-                    ) : (
-                        <React.Fragment>
-                            <div className="bg"></div>
-                            <div className="textCover">
-                                <div className="title" style={{fontSize: fontSize+'px'}}>{item.title}</div>
-                                {(item.row <= 3) ? (
-                                    <div className="author">{item.author}</div>
-                                ) : null}
-                            </div>
-                        </React.Fragment>
-                    )}
-                </div>
-            </React.Fragment>
+            <div className={'item'} ref="item" id={item.id} style={{
+                width: item.width+'px',
+                height: item.height+'px',
+                cursor: 'pointer',
+                position: 'relative',
+                display: 'inline-flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+              
+            }} onClick={this.onClick.bind(this)}>
+                {this.props.item.cover ? (
+                    <img src={item.cover} alt={item.title} ref="cover" style={{height: '100%'}} />
+                ) : (
+                    <React.Fragment>
+                        <div className="bg"></div>
+                        <div className="textCover">
+                            <div className="title" style={{fontSize: fontSize+'px'}}>{item.title}</div>
+                            {(item.row <= 3) ? (
+                                <div className="author">{item.author}</div>
+                            ) : null}
+                        </div>
+                    </React.Fragment>
+                )}
+            </div>
         );
     }
 }
