91da0354a0313fa76c29fd14580259266472b9c8
deguchi <deguchik@gmail.com>
2021-05-25 11:44:20 +0900

    update

--- example/component/DefaultItem.tsx
+++ example/component/DefaultItem.tsx
@@ -29,18 +29,20 @@ export default class Item extends Component<Props, State> {
         const item = this.props.item
         const fontSize = (this.props.item.width - this.props.margin) / 14
         const sortKeySize = this.props.item.height / 7
+        const width = item.width - this.props.margin
+        const height = item.height - this.props.margin
         return (
             <div className={'hiradumiBook' + (!this.props.item.cover ? ' nocover' : '')} id={item.id} style={{
                 display: 'inline-block',
-                width: item.width - this.props.margin + 'px',
-                height: item.height - this.props.margin + 'px',
+                width: width + 'px',
+                height: height + 'px',
                 margin: this.props.margin / 2 + 'px',
             }}>
                 <a href={'https://calil.jp/book/' + this.props.item.isbn} target="_blank" style={{
                     display: 'inline-block',
-                    width: item.width - this.props.margin + 'px',
-                    height: item.height - this.props.margin + 'px'
-                }}>
+                    width: width + 'px',
+                    height: height + 'px',
+                    }}>
                 {this.props.item.cover ? (
                     <React.Fragment>
                         <img src={item.cover}
@@ -48,11 +50,11 @@ export default class Item extends Component<Props, State> {
                             data-aspect={item.properties.aspect}
                             ref="cover"
                             style={{
-                                height: item.height - this.props.margin + 'px',
-                                width: item.width - this.props.margin + 'px'
+                                width: width + 'px',
+                                height: height + 'px',
                             }}
                         />
-                        {this.props.sortKey ? (
+                        {this.props.sortKey && height > 100 ? (
                             <span className="sortKey" style={{
                                 opacity: item[this.props.sortKey] / 4 + 0.1,
                                 fontSize: fontSize + 'px',
@@ -67,10 +69,14 @@ export default class Item extends Component<Props, State> {
                     <React.Fragment>
                         <div className="bg"></div>
                         <div className="textCover">
-                            <div className="title" style={{fontSize: fontSize+'px'}}>{item.title}</div>
-                            <div className="author" style={{fontSize: fontSize*0.7+'px'}}>{item.author}</div>
+                            {height > 100 ? (
+                            <React.Fragment>
+                                <div className="title" style={{fontSize: fontSize+'px'}}>{item.title}</div>
+                                <div className="author" style={{fontSize: fontSize*0.7+'px'}}>{item.author}</div>
+                            </React.Fragment>
+                            ) : null}
                         </div>
-                        {this.props.sortKey ? (
+                        {this.props.sortKey && height > 100 ? (
                             <span className="sortKey" style={{
                                 opacity: item[this.props.sortKey] / 4 + 0.1,
                                 fontSize: fontSize + 'px',
