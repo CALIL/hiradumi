c5426d4fffd9f90735280ecab90d7913da454e17
deguchi <deguchik@gmail.com>
2021-06-18 10:31:55 +0900

    update

--- example/index.tsx
+++ example/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App';
 
-fetch('https://nxc.calil.dev/bookfair/all.json').then((r) => r.json()).then((r) => {
+fetch('https://bookdata-fair.calil.dev/all.json').then((r) => r.json()).then((r) => {
     ReactDOM.render(<App items={r} />, document.getElementById('root'));
 })
