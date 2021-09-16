72b8092b686cbbdca3b77ec4c413d1a7b2196182
deguchi <deguchik@gmail.com>
2021-09-16 15:00:45 +0900

    update

--- src/Hiradumi.tsx
+++ src/Hiradumi.tsx
@@ -19,6 +19,62 @@ function getScrollbarWidth() {
     return scrollBarWidth
 }
 
+// 行の幅の範囲内にアイテムを入れる
+const putItem = (currentItems: any[], rowWidth: number, rowRatio: number, itemHeight: number, itemMargin: number) => {
+    const items:any[] = []
+    let rowTotalWidth = 0
+    
+    const height = itemHeight * rowRatio
+    currentItems.some((item) => {
+        const hasAspect = item.properties && item.properties.aspect
+        const aspect: number = hasAspect ? item.properties.aspect : 0.666666
+        const width = Math.floor(height * aspect) + itemMargin
+        // 行よりも大きくなるなら終了
+        if (rowTotalWidth + width > rowWidth) return true
+        item.height = height
+        item.width = width
+        items.push(item)
+        rowTotalWidth += width
+    })
+    return items
+}
+
+// 前の行をrowTotalWidth分詰めて、今の行を押し込む
+const pushPreviousRow = (items: any[], prevItems: any[], rowTotalWidth: number) => {
+    const prevRowTotalWidth = prevItems.reduce((size, item) => size + item.width, 0)
+    const scaleDownRatio = prevRowTotalWidth / (prevRowTotalWidth + rowTotalWidth)
+    prevItems.some((item) => {
+        item.width = Math.floor(item.width * scaleDownRatio)
+        item.height = Math.floor(item.height * scaleDownRatio)
+    })
+    // 今の行のサイズを調整
+    const scaleRatio = prevItems[0].height / items[0].height
+    items.some((item) => {
+        item.width = Math.floor(item.width * scaleRatio)
+        item.height = Math.floor(item.height * scaleRatio)
+    })
+    return prevItems.concat(items)
+}
+
+// sortKeyでソートして、中央から並べ直す
+const sortCenter = (items: any[], sortLabel: string) => {
+    if (sortLabel) {
+        items.sort(function (a, b) {
+            if (a[sortLabel] < b[sortLabel]) return 1
+            if (a[sortLabel] > b[sortLabel]) return -1
+            return 0
+        })
+    }
+    const newRowData: any = {}
+    const centerIndex = Math.floor(items.length / 2)
+    newRowData[centerIndex] = items[0]
+    Array.from({ length: centerIndex }).forEach((item, index) => {
+        if (items[index * 2 + 1]) newRowData[centerIndex - 1 - index] = items[index * 2 + 1]
+        if (items[index * 2 + 2]) newRowData[centerIndex + 1 + index] = items[index * 2 + 2]
+    })
+    return Object.values(newRowData)
+}
+
 interface Props {
     width: number
     height: number
@@ -55,137 +111,6 @@ const Hiradumi = (props: Props) => {
     let HiradumiDiv: any = null
     let PrevScrollTo = {key: null, value: null}
 
-    // 行の幅の範囲内にアイテムを入れる
-    const putItem = (currentItems: any[], rowWidth: number, rowRatio: number) => {
-        const items:any[] = []
-        let rowTotalWidth = 0
-        
-        const height = props.itemHeight * rowRatio
-        currentItems.some((item) => {
-            const hasAspect = item.properties && item.properties.aspect
-            const aspect: number = hasAspect ? item.properties.aspect : 0.666666
-            const width = Math.floor(height * aspect) + props.itemMargin
-            // 行よりも大きくなるなら終了
-            if (rowTotalWidth + width > rowWidth) return true
-            item.height = height
-            item.width = width
-            items.push(item)
-            rowTotalWidth += width
-        })
-        return items
-    }
-
-
-    // 前の行をrowTotalWidth分詰めて、今の行を押し込む
-    const pushPreviousRow = (items: any[], prevItems: any[], rowTotalWidth: number) => {
-        const prevRowTotalWidth = prevItems.reduce((size, item) => size + item.width, 0)
-        const scaleDownRatio = prevRowTotalWidth / (prevRowTotalWidth + rowTotalWidth)
-        prevItems.some((item) => {
-            item.width = Math.floor(item.width * scaleDownRatio)
-            item.height = Math.floor(item.height * scaleDownRatio)
-        })
-        // 今の行のサイズを調整
-        const scaleRatio = prevItems[0].height / items[0].height
-        items.some((item) => {
-            item.width = Math.floor(item.width * scaleRatio)
-            item.height = Math.floor(item.height * scaleRatio)
-        })
-        return prevItems.concat(items)
-    }
-
-    // sortKeyでソートして、中央から並べ直す
-    const sortCenter = (items: any[]) => {
-        const sortLabel = props.sortKey
-        if (sortLabel) {
-            items.sort(function (a, b) {
-                if (a[sortLabel] < b[sortLabel]) return 1
-                if (a[sortLabel] > b[sortLabel]) return -1
-                return 0
-            })
-        }
-        const newRowData: any = {}
-        const centerIndex = Math.floor(items.length / 2)
-        newRowData[centerIndex] = items[0]
-        Array.from({ length: centerIndex }).forEach((item, index) => {
-            if (items[index * 2 + 1]) newRowData[centerIndex - 1 - index] = items[index * 2 + 1]
-            if (items[index * 2 + 2]) newRowData[centerIndex + 1 + index] = items[index * 2 + 2]
-        })
-        return Object.values(newRowData)
-    }
-
-    const setRowData = () => {
-        let rowHeights: number[] = []
-        let rows: any[] = []
-        let prevRowItems: any[] = []
-
-        if (props.headerComponent) {
-            rows.push({type: 'header', component: props.headerComponent})
-            rowHeights.push(props.headerHeight)
-        }
-
-        // 計算しているitemのindex
-        let itemIndex = 0
-        let items = Items.slice(itemIndex, itemIndex+100)
-
-        const rowMaxWidth = props.width - props.padding * 2 - getScrollbarWidth()
-        for (let i = 0; items.length > 0 && rows.length < props.rowCount; i++) {
-            // 行数制限
-            if (props.rowCount === rows.length - 1) return true
-
-            const rowRatioIndex = i % props.rowRatios.length
-            const rowRatio = props.rowRatios[rowRatioIndex]
-
-            // 行の幅の範囲内にアイテムを入れる
-            let rowItems = putItem(items, rowMaxWidth, rowRatio)
-
-            const rowWidth = rowItems.reduce((size, item) => size + item.width, 0)
-
-            // 残りの横幅分、サイズを調整
-            const scaleUpRatio = rowMaxWidth / rowWidth
-
-            // 縦が大きすぎないように規制 && 最後の行が1冊以上の場合
-            if (scaleUpRatio < 2 && rowItems.length > 1) {
-                rowItems.some((item) => {
-                    item.width = Math.floor(item.width * scaleUpRatio)
-                    item.height = Math.floor(item.height * scaleUpRatio)
-                })
-            } else {
-                if (prevRowItems.length > 0) {
-                    // 前の行をrowTotalWidth分詰めて、今の行を押し込む
-                    rowItems = pushPreviousRow(rowItems, prevRowItems, rowWidth)
-                    rows.pop()
-                    rowHeights.pop()
-                }
-            }
-
-            // sortKeyでソートして、中央から並べ直す
-            if (props.sortKey) rowItems = sortCenter(rowItems)
-
-            rows.push(rowItems)
-            prevRowItems = rowItems
-            
-            // 行の高さを割り出してセット
-            let heights:number[] = []
-            rowItems.forEach((item) => {
-                heights.push(item.height + props.itemMargin)
-            })
-            rowHeights.push(Math.max(...heights))
-    
-            itemIndex += rowItems.length
-            items = Items.slice(itemIndex, itemIndex+100)
-
-        }
-
-        if (props.footerComponent) {
-            rows.push({type: 'footer', component: props.footerComponent})
-            rowHeights.push(props.footerHeight)
-        }
-
-        Rows = rows
-        RowHeights = rowHeights
-
-    }
-
     const onScroll = (event: any) => {
         if (!HiradumiDiv) return
         const scrollArea = HiradumiDiv.firstElementChild
@@ -194,39 +119,30 @@ const Hiradumi = (props: Props) => {
         props.onScroll(event)
     }
 
-    const scrollTo = (key: string, value: any) => {
+    const hiradumiScrollTo = (key: string, value: any) => {
         let index: number
-        Rows.forEach((items, i) => {
+        // 対象となあるitemを探す
+        Rows.some((items, i) => {
             if (typeof items.type==='undefined' && items.filter((item: any)=> item[key] === value).length > 0) {
                 index = i
+                return true
             }
         })
-        let height = props.padding
+        // スクロール量を計算
+        let scrollHeight = props.padding
         RowHeights.some((rowHeight, i) => {
             if (i===index) return true
-            height += rowHeight
+            scrollHeight += rowHeight
         })
+        // 表示item数が多い場合のためにスクロールを何度か行う
         const timer = setInterval(() => {
-            if (HiradumiDiv.firstElementChild.scrollTop >= height) {
-                return clearTimeout(timer)
-            }
+            if (HiradumiDiv.firstElementChild.scrollTop >= scrollHeight) return clearTimeout(timer)
             // console.log(HiradumiDiv.firstElementChild.scrollTop)
-            HiradumiDiv.firstElementChild.scrollTo(0, height)
+            HiradumiDiv.firstElementChild.scrollTo(0, scrollHeight)
         }, 1)
-        setTimeout(() => {
-            clearTimeout(timer)
-        }, 1000)
+        setTimeout(() => clearTimeout(timer), 1000)
     }
 
-    const checkScrollTo = () => {
-        if (props.scrollTo && props.scrollTo.value !== PrevScrollTo.value) {
-            const {key, value} = props.scrollTo
-            setTimeout(() => {
-                scrollTo(key, value)
-            }, 100)
-            PrevScrollTo = JSON.parse(JSON.stringify(props.scrollTo))
-        }
-    }
 
     const renderRow = (row: any, style: any) => {
         const rowStyle = JSON.parse(JSON.stringify(style))
@@ -270,8 +186,84 @@ const Hiradumi = (props: Props) => {
         }
     }
 
-    setRowData()
-    checkScrollTo()
+    let rowHeights: number[] = []
+    let rows: any[] = []
+    let prevRowItems: any[] = []
+
+    if (props.headerComponent) {
+        rows.push({type: 'header', component: props.headerComponent})
+        rowHeights.push(props.headerHeight)
+    }
+
+    // 計算しているitemのindex
+    let itemIndex = 0
+    let items = Items.slice(itemIndex, itemIndex+100)
+
+    const rowMaxWidth = props.width - props.padding * 2 - getScrollbarWidth()
+    for (let i = 0; items.length > 0 && rows.length < props.rowCount; i++) {
+        // 行数制限
+        if (props.rowCount === rows.length - 1) return true
+
+        const rowRatioIndex = i % props.rowRatios.length
+        const rowRatio = props.rowRatios[rowRatioIndex]
+
+        // 行の幅の範囲内にアイテムを入れる
+        let rowItems = putItem(items, rowMaxWidth, rowRatio, props.itemHeight, props.itemMargin)
+
+        const rowWidth = rowItems.reduce((size, item) => size + item.width, 0)
+
+        // 残りの横幅分、サイズを調整
+        const scaleUpRatio = rowMaxWidth / rowWidth
+
+        // 縦が大きすぎないように規制 && 最後の行が1冊以上の場合
+        if (scaleUpRatio < 2 && rowItems.length > 1) {
+            rowItems.some((item) => {
+                item.width = Math.floor(item.width * scaleUpRatio)
+                item.height = Math.floor(item.height * scaleUpRatio)
+            })
+        } else {
+            if (prevRowItems.length > 0) {
+                // 前の行をrowTotalWidth分詰めて、今の行を押し込む
+                rowItems = pushPreviousRow(rowItems, prevRowItems, rowWidth)
+                rows.pop()
+                rowHeights.pop()
+            }
+        }
+
+        // sortKeyでソートして、中央から並べ直す
+        if (props.sortKey) rowItems = sortCenter(rowItems, props.sortKey)
+
+        rows.push(rowItems)
+        prevRowItems = rowItems
+        
+        // 行の高さを割り出してセット
+        let heights:number[] = []
+        rowItems.forEach((item) => {
+            heights.push(item.height + props.itemMargin)
+        })
+        rowHeights.push(Math.max(...heights))
+
+        itemIndex += rowItems.length
+        items = Items.slice(itemIndex, itemIndex+100)
+
+    }
+
+    if (props.footerComponent) {
+        rows.push({type: 'footer', component: props.footerComponent})
+        rowHeights.push(props.footerHeight)
+    }
+
+    Rows = rows
+    RowHeights = rowHeights
+
+    // スクロールするか判定
+    if (props.scrollTo && props.scrollTo.value !== PrevScrollTo.value) {
+        const {key, value} = props.scrollTo
+        setTimeout(() => {
+            hiradumiScrollTo(key, value)
+        }, 100)
+        PrevScrollTo = JSON.parse(JSON.stringify(props.scrollTo))
+    }
 
     return (<div className={props.className ? props.className : 'hiradumi'} ref={(element) => HiradumiDiv = element}>
         <List
