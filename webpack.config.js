0c54f512142c92a52ae38bcca06c76c7471e3995
deguchi <deguchik@gmail.com>
2021-09-17 15:08:35 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -20,3 +20,24 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.jsx', '.js']
     },
 }
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    // devtool: 'inline-source-map',
+    entry: './DefaultItem.tsx',
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    output: {
+        filename: './DefaultItem.js',
+        path: path.resolve(__dirname, 'lib')
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js']
+    },
+}
