d32abe6d07990b5b682e5f19315d5977fd9a017e
deguchi <deguchik@gmail.com>
2022-04-08 10:28:44 +0900

    不要ファイル削除

--- demo/component/Item.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-interface Props {
-    item: any;
-    margin: number;
-    sortKey: string | null;
-}
-declare const Item: (props: Props) => JSX.Element;
-export default Item;
--- example/src/App.d.ts
+++
@@ -1,2 +0,0 @@
-declare const HiradumiView: (props: any) => JSX.Element;
-export default HiradumiView;
