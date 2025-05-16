6758eb4844441c969fa1d3776a3a5e696a304afe
Satoshi Deguchi <deguchik@gmail.com>
2025-05-16 09:45:02 +0900

    update

--- src/layoutCalculator.ts
+++ src/layoutCalculator.ts
@@ -1,12 +1,8 @@
-// 行の高さの基準値(itemHeight)に、各行の比率(rowRatio)をかけて、行の高さを決める
-// 行の比率(rowRatios)の数だけ、行をつくる
-// Itemをaspect比をもとに横幅を決めて行に詰めていく
-// この繰り返しで、行を表示する
+// 行の高さの基準値(itemHeight)に、各行の比率(itemScales)をかけて、行の高さを決める
+// Itemの横幅を行の高さとaspect比をもとにを決めて行に入れていく
+// 行の横幅がwidthを超えたら、行の高さを基準値に戻して、次の行を作る
 // 各Itemのaspect比は違うので、行の高さのパターンは繰り返しでも違って見える
 // これによって、一定の見た目のランダム性、平積み感を出す
-
-import { it } from "vitest"
-
 
 export type ItemType = {
     type: 'text' | 'image'
@@ -14,7 +10,7 @@ export type ItemType = {
     isbn?: string
     width: number
     height: number
-    aspect?: number
+    aspect: number
 }
 
 
@@ -80,30 +76,23 @@ export const layoutCalculator = (items: ItemType[], options: LayoutOptions): Ite
 
     // 最後の行の処理
     if (rowItems.length > 0) {
-        if (rowItems.length > 2) {
+        // 最後の行の項目が3以下の場合、1つ前の行に入れて調整する
+        if (rowItems.length <= 3) {
+            rowItems.forEach((rowItem) => {
+                rowItem.height = prevRowItems[0].height
+                rowItem.width = rowItem.height * rowItem.aspect
+            })
+            const lastRowItems = prevRowItems.concat(rowItems)
+            const lastRowWidth = lastRowItems.reduce((acc, item) => acc + item.width, 0)
+            lastRowItems.forEach((rowItem) => {
+                setItemSize(rowItem, itemHeight, width / lastRowWidth)
+            })
+            adjustItemWidth(lastRowItems, width)
+        } else {
             rowItems.forEach((rowItem) => {
                 setItemSize(rowItem, itemHeight, width / rowWidth)
             })
             adjustItemWidth(rowItems, width)
-        } else {
-            rowItems.forEach((rowItem) => {
-                rowItem.width = prevRowItems[0].width
-                rowItem.height = prevRowItems[0].height
-            })
-            const lastRowItems = prevRowItems.concat(rowItems)
-            const lastRowWidth = lastRowItems.reduce((acc, item) => acc + item.width, 0)
-            console.log('lastRowWidth', lastRowWidth)
-            console.log('width', width)
-            lastRowItems.forEach((rowItem) => {
-                setItemSize(rowItem, itemHeight, width / lastRowWidth)
-            })
-            const diffWidth = lastRowItems.reduce((acc, item) => acc + item.width, 0)
-            const adjustPixelForFirefox = 0.5 // Firefoxのクセによる調整
-            const addWidth = roundToDecimals((width - diffWidth - adjustPixelForFirefox) / lastRowItems.length)
-            lastRowItems.forEach((item) => {
-                item.width += addWidth
-                item.height = item.width / item.aspect
-            })
         }
     }
 
@@ -146,10 +135,11 @@ export const setItemSize = (item: ItemType, itemHeight: number, ratio: number): 
  */
 const adjustItemWidth = (items: ItemType[], width: number): void => {
     const diffWidth = items.reduce((acc, item) => acc + item.width, 0)
-    const adjustPixelForFirefox = 0.5 // Firefoxのクセによる調整
+    const adjustPixelForFirefox = 0.05 * items.length // Firefoxのクセを考慮
     const addWidth = roundToDecimals((width - diffWidth - adjustPixelForFirefox) / items.length)
     items.forEach((item) => {
         item.width += addWidth
+        item.height = item.width / item.aspect
     })
 }
 
